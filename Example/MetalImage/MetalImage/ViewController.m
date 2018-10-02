2943a0adb5635775411340e2bc022a438f072927
你见唔到我 <403482009@qq.com>
2018-10-02 12:47:38 +0800

    Adjust File Struct

--- Example/MetalImage/MetalImage/ViewController.m
+++ Example/MetalImage/MetalImage/ViewController.m
@@ -47,42 +47,42 @@
                                             displayViewWidth,
                                             (int)(displayViewWidth * 4.0/3));
     
-//    MIFilter *filter1 = [[MIFilter alloc]init];
-//    MIHueFilter *filter2 = [[MIHueFilter alloc]init];
-//    MIHueFilter *filter3 = [[MIHueFilter alloc]init];
-//    MISaturationFilter *filter4 = [[MISaturationFilter alloc]init];
-//    filter4.saturation = 2.0f;
-//    MIHueFilter *filter5 = [[MIHueFilter alloc]init];
-//    filter5.hue  = 180.0f;
-//    MIFilter *filter6 = [[MIFilter alloc]init];
-//    MIFilter *filter7 = [[MIFilter alloc]init];
-//    MIFilter *filter8 = [[MIFilter alloc]init];
-//    MIFilter *filter9 = [[MIFilter alloc]init];
-//    MIFilter *filter10 = [[MIFilter alloc]init];
-//    MIFilter *filter11 = [[MIFilter alloc]init];
-//    MIFilter *filter12 = [[MIFilter alloc]init];
-//
-//    [_videoCaptor addConsumer:_defaultFilter];
-//    [_defaultFilter addConsumer:filter1];
-//    [filter1 addConsumer:filter2];
-//    [filter2 addConsumer:filter3];
-//    [filter3 addConsumer:filter4];
-//    [filter4 addConsumer:filter5];
-//    [filter5 addConsumer:filter6];
-//    [filter6 addConsumer:filter7];
-//    [filter7 addConsumer:filter8];
-//    [filter8 addConsumer:filter9];
-//    [filter9 addConsumer:filter10];
-//    [filter10 addConsumer:filter11];
-//    [filter11 addConsumer:filter12];
-//    [filter12 addConsumer:_displayView];
+    //    MIFilter *filter1 = [[MIFilter alloc]init];
+    //    MIHueFilter *filter2 = [[MIHueFilter alloc]init];
+    //    MIHueFilter *filter3 = [[MIHueFilter alloc]init];
+    //    MISaturationFilter *filter4 = [[MISaturationFilter alloc]init];
+    //    filter4.saturation = 2.0f;
+    //    MIHueFilter *filter5 = [[MIHueFilter alloc]init];
+    //    filter5.hue  = 180.0f;
+    //    MIFilter *filter6 = [[MIFilter alloc]init];
+    //    MIFilter *filter7 = [[MIFilter alloc]init];
+    //    MIFilter *filter8 = [[MIFilter alloc]init];
+    //    MIFilter *filter9 = [[MIFilter alloc]init];
+    //    MIFilter *filter10 = [[MIFilter alloc]init];
+    //    MIFilter *filter11 = [[MIFilter alloc]init];
+    //    MIFilter *filter12 = [[MIFilter alloc]init];
+    //
+    //    [_videoCaptor addConsumer:_defaultFilter];
+    //    [_defaultFilter addConsumer:filter1];
+    //    [filter1 addConsumer:filter2];
+    //    [filter2 addConsumer:filter3];
+    //    [filter3 addConsumer:filter4];
+    //    [filter4 addConsumer:filter5];
+    //    [filter5 addConsumer:filter6];
+    //    [filter6 addConsumer:filter7];
+    //    [filter7 addConsumer:filter8];
+    //    [filter8 addConsumer:filter9];
+    //    [filter9 addConsumer:filter10];
+    //    [filter10 addConsumer:filter11];
+    //    [filter11 addConsumer:filter12];
+    //    [filter12 addConsumer:_displayView];
     
     
     
     [_videoCaptor addConsumer:_defaultFilter];
     [_defaultFilter addConsumer:_displayView];
     [_videoCaptor startRunning];
-
+    
     
 }
 
--- Example/MetalImage/MetalImage/main.m
+++ Example/MetalImage/MetalImage/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  MetalImage
 //
-//  Created by Sylar on 2018/9/3.
+//  Created by Sylar on 2018/10/2.
 //  Copyright © 2018年 Sylar. All rights reserved.
 //
 
